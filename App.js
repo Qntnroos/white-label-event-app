aaf136eb21527286c9e7bf43902b3576e92b7e77
Kevin Meyvaert <kevin.meyvaert@inthepocket.mobi>
2017-10-03 13:23:20 +0200

    Removed unused code

--- App.js
+++ App.js
@@ -88,8 +88,6 @@ export default class App extends Component {
         <Navigator
           screenProps={{
             shiftData: this.state.shiftData,
-
-            subscribe: trackId => subscribeToTrack(trackId, 'jonathan'),
             userInfo,
             login: () => this.handleUserLogin(),
 
